cac3c6f78c20dabab2856004d7f7ce99d3d9ab77
Krit Gupta <kritgupta@tamu.edu>
2018-04-21 20:32:15 +0000

    first

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var path = require('path');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+///////////////////////////////////LANDING PAGE////////////////////////////////////////
+app.get("/", function(req, res){
+    res.render("landing.ejs");
+});
+
+
+
+
+
+///////////////////////////////////GRAPH PAGE/////////////////////////////////////////
+app.get("/graph", function(req, res){
+    res.sendFile(path.join(__dirname + '/graph2.html'));
+});
+
+
+app.get("/line", function(req, res){
+    res.sendFile(path.join(__dirname + '/line.html'));
+});
+
+app.get("/line2", function(req, res){
+    res.sendFile(path.join(__dirname + '/line2.html'));
+});
+
+
+app.get("/slide", function(req, res){
+    res.sendFile(path.join(__dirname + '/slide.html'));
+});
+
+
+/////////////////////////////////////////SERVER///////////////////////////////////////////////////
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Server has started!");
+});
